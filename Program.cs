05b38b3325bdbe4034d9ed2211f799ca34114fa3
Brage Hamre Skjørestad <brage.skjorestad@gmail.com>
2026-01-24 12:40:24 +0100

    try prod build again

--- Program.cs
+++ Program.cs
@@ -24,17 +24,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+if (!string.IsNullOrEmpty(connectionString))
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "MediaArchive API is running! Try /WeatherForecast or /swagger");
 
 app.UseHttpsRedirection();
 
